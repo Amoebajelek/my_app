91cbd7cb09a164518f0bc43bd52402cb30d5424e
Naufal Abdussyakur <76672769+Amoebajelek@users.noreply.github.com>
2023-04-28 22:41:27 +0700

    Add Riverpod Dependency

---
+++ lib/main_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_app/provider.dart';
+
+class MainScreen extends ConsumerWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              ref.watch(nameProvider),
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/provider.dart
@@ -0,0 +1,3 @@
+import 'package:riverpod/riverpod.dart';
+
+final nameProvider = Provider((ref) => 'Naufal Abdussyakur');
